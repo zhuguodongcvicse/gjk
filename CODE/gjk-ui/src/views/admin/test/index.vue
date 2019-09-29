07caca445feaac9e9b060dcb9a5abada6768bcad
geng_hxian <ghxhuixian@163.com>
2019-09-29 13:51:04 +0800

    修改库目录结构管理的不选这届带你直接添加报错问题

--- CODE/gjk-ui/src/views/admin/test/index.vue
+++ CODE/gjk-ui/src/views/admin/test/index.vue
@@ -3,63 +3,46 @@
     <basic-container>
       <div class="filter-container">
         <el-button-group>
-          <el-button type="primary"
-                     v-if="testManager_btn_add"
-                     icon="plus"
-                     @click="handlerAdd">添加
-          </el-button>
-          <el-button type="primary"
-                     v-if="testManager_btn_edit"
-                     icon="edit"
-                     @click="handlerEdit">编辑
-          </el-button>
-          <el-button type="primary"
-                     v-if="testManager_btn_del"
-                     icon="delete"
-                     @click="handleDelete">删除
-          </el-button>
+          <el-button type="primary" v-if="testManager_btn_add" icon="plus" @click="handlerAdd">添加</el-button>
+          <el-button type="primary" v-if="testManager_btn_edit" icon="edit" @click="handlerEdit">编辑</el-button>
+          <el-button
+            type="primary"
+            v-if="testManager_btn_del"
+            icon="delete"
+            @click="handleDelete"
+          >删除</el-button>
         </el-button-group>
       </div>
 
       <el-row class="admin_menu_index_main_14s">
         <el-col :span="5" class="menu_main_left_14s">
-          <el-tree class="filter-tree"
-                   node-key="id"
-                   highlight-current
-                   :data="treeData"
-                   :default-expanded-keys="aExpandedKeys"
-                   :filter-node-method="filterNode"
-                   :props="defaultProps"
-                   @node-click="getNodeData"
-                   @node-expand="nodeExpand"
-                   @node-collapse="nodeCollapse"
-                   @node-drop="handleDrop"
-                   draggable
-                   >
-          </el-tree>
+          <el-tree
+            class="filter-tree"
+            node-key="id"
+            highlight-current
+            :data="treeData"
+            :default-expanded-keys="aExpandedKeys"
+            :filter-node-method="filterNode"
+            :props="defaultProps"
+            @node-click="getNodeData"
+            @node-expand="nodeExpand"
+            @node-collapse="nodeCollapse"
+            @node-drop="handleDrop"
+            draggable
+          ></el-tree>
         </el-col>
         <el-col :span="16" class="menu_main_right_14s">
           <el-card class="box-card">
-            <el-form :label-position="labelPosition"
-                     label-width="80px"
-                     :model="form"
-                     ref="form">             
-              <el-form-item label="测试库名"
-                            prop="name">
-                <el-input v-model="form.name"
-                          :disabled="formEdit"
-                          placeholder="请输入测试库名"></el-input>
-              </el-form-item>                    
+            <el-form :label-position="labelPosition" label-width="80px" :model="form" ref="form">
+              <el-form-item label="测试库名" prop="name">
+                <el-input v-model="form.name" :disabled="formEdit" placeholder="请输入测试库名"></el-input>
+              </el-form-item>
               <el-form-item v-if="formStatus == 'update'">
-                <el-button type="primary"
-                           @click="update">更新
-                </el-button>
+                <el-button type="primary" @click="update">更新</el-button>
                 <el-button @click="onCancel">取消</el-button>
               </el-form-item>
               <el-form-item v-if="formStatus == 'create'">
-                <el-button type="primary"
-                           @click="create">保存
-                </el-button>
+                <el-button type="primary" @click="create">保存</el-button>
                 <el-button @click="onCancel">取消</el-button>
               </el-form-item>
             </el-form>
@@ -71,214 +54,222 @@
 </template>
 
 <script>
-  import {addObj, delObj, fetchTestTree, getObj, putObj} from '@/api/admin/test'
-  import {mapGetters} from 'vuex'
+import {
+  addObj,
+  delObj,
+  fetchTestTree,
+  getObj,
+  putObj
+} from "@/api/admin/test";
+import { mapGetters } from "vuex";
 
-  export default {
-    name: 'test',
-    data() {
-      return {
-        list: null,
-        total: null,
-        formEdit: true,
-        formAdd: true,
-        formStatus: '',
-        showElement: false,
-        typeOptions: ['0', '1'],
-        methodOptions: ['GET', 'POST', 'PUT', 'DELETE'],
-        listQuery: {
-          name: undefined
-        },
-        treeData: [],
-        oExpandedKey: {
-          // key (from tree id) : expandedOrNot boolean
-        },
-        oTreeNodeChildren: {
-          // id1 : [children] (from tree node id1)
-          // id2 : [children] (from tree node id2)
-        },
-        aExpandedKeys: [],
-        defaultProps: {
-          children: 'children',
-          label: 'name'
-        },
-        rules: {
-          name: [
-            {required: true, message: '请输入测试库名', trigger: 'blur'}
-          ],
-        },
-        labelPosition: 'right',
-        form: {
-          name: undefined,
-          testId: undefined,
-          parentId: undefined,
-          sort: undefined
-        },
-        currentId: -1,
-        testManager_btn_add: false,
-        testManager_btn_edit: false,
-        testManager_btn_del: false
+export default {
+  name: "test",
+  //刷新
+  inject: ["reload"],
+  data() {
+    return {
+      list: null,
+      total: null,
+      formEdit: true,
+      formAdd: true,
+      formStatus: "",
+      showElement: false,
+      typeOptions: ["0", "1"],
+      methodOptions: ["GET", "POST", "PUT", "DELETE"],
+      listQuery: {
+        name: undefined
+      },
+      treeData: [],
+      oExpandedKey: {
+        // key (from tree id) : expandedOrNot boolean
+      },
+      oTreeNodeChildren: {
+        // id1 : [children] (from tree node id1)
+        // id2 : [children] (from tree node id2)
+      },
+      aExpandedKeys: [],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      },
+      rules: {
+        name: [{ required: true, message: "请输入测试库名", trigger: "blur" }]
+      },
+      labelPosition: "right",
+      form: {
+        name: undefined,
+        testId: undefined,
+        parentId: undefined,
+        sort: undefined
+      },
+      currentId: -1,
+      testManager_btn_add: false,
+      testManager_btn_edit: false,
+      testManager_btn_del: false
+    };
+  },
+
+  created() {
+    this.getList();
+    this.testManager_btn_add = this.permissions["sys_test_add"];
+    this.testManager_btn_edit = this.permissions["sys_test_edit"];
+    this.testManager_btn_del = this.permissions["sys_test_del"];
+  },
+  computed: {
+    ...mapGetters(["elements", "permissions"])
+  },
+  methods: {
+    handleDrop(draggingNode, dropNode, dropType, ev) {
+      this.rules.name[0].required = false;
+      if (dropType === "inner") {
+        var parentId = dropNode.data.id;
+        var testId =
+          dropNode.data.children[dropNode.data.children.length - 1].id;
+        var name =
+          dropNode.data.children[dropNode.data.children.length - 1].name;
+      } else if (dropType === "before") {
+        var parentId = dropNode.data.parentId;
+        var testId = draggingNode.data.id;
+        var name = draggingNode.data.name;
+      } else if (dropType === "after") {
+        var parentId = dropNode.data.parentId;
+        var testId = draggingNode.data.id;
+        var name = draggingNode.data.name;
+      }
+      this.form = {
+        name: name,
+        sort: undefined,
+        parentId: parentId,
+        testId: testId
+      };
+      this.update();
+      this.rules.name[0].required = true;
+    },
+
+    getList() {
+      fetchTestTree(this.listQuery).then(response => {
+        this.treeData = response.data.data;
+      });
+    },
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.label.indexOf(value) !== -1;
+    },
+
+    nodeExpand(data) {
+      let aChildren = data.children;
+      if (aChildren.length > 0) {
+        this.oExpandedKey[data.id] = true;
+        this.oTreeNodeChildren[data.id] = aChildren;
+      }
+      this.setExpandedKeys();
+    },
+    nodeCollapse(data) {
+      this.oExpandedKey[data.id] = false;
+      // 如果有子节点
+      this.treeRecursion(this.oTreeNodeChildren[data.id], oNode => {
+        this.oExpandedKey[oNode.id] = false;
+      });
+      this.setExpandedKeys();
+    },
+    setExpandedKeys() {
+      let oTemp = this.oExpandedKey;
+      this.aExpandedKeys = [];
+      for (let sKey in oTemp) {
+        if (oTemp[sKey]) {
+          this.aExpandedKeys.push(parseInt(sKey));
+        }
       }
     },
-    
-    created() {
-      this.getList()
-      this.testManager_btn_add = this.permissions['sys_test_add']
-      this.testManager_btn_edit = this.permissions['sys_test_edit']
-      this.testManager_btn_del = this.permissions['sys_test_del']
+    treeRecursion(aChildren, fnCallback) {
+      if (aChildren) {
+        for (let i = 0; i < aChildren.length; ++i) {
+          let oNode = aChildren[i];
+          fnCallback && fnCallback(oNode);
+          this.treeRecursion(oNode.children, fnCallback);
+        }
+      }
     },
-    computed: {
-      ...mapGetters([
-        'elements',
-        'permissions'
-      ])
+
+    getNodeData(data) {
+      if (!this.formEdit) {
+        this.formStatus = "update";
+      }
+      getObj(data.id).then(response => {
+        this.form = response.data.data;
+      });
+      this.currentId = data.id;
+      this.showElement = true;
     },
-    methods: {
-      handleDrop(draggingNode, dropNode, dropType, ev) {
-        this.rules.name[0].required=false;
-        if(dropType==="inner"){
-            var parentId = dropNode.data.id;
-            var testId = dropNode.data.children[dropNode.data.children.length-1].id;
-            var name = dropNode.data.children[dropNode.data.children.length-1].name;
-           
-        }else if(dropType==="before"){
-            var parentId = dropNode.data.parentId;
-            var testId = draggingNode.data.id;
-            var name = draggingNode.data.name;
-        }else if(dropType==="after"){
-            var parentId = dropNode.data.parentId;
-            var testId = draggingNode.data.id;
-            var name = draggingNode.data.name;
-        }
-        this.form={
-                name: name,
-                sort: undefined,
-                parentId: parentId,
-                testId: testId
-        }
-        this.update()
-        this.rules.name[0].required=true;
-      },
-
-
-      getList() {
-        fetchTestTree(this.listQuery).then(response => {
-          this.treeData = response.data.data
-        })
-      },
-      filterNode(value, data) {
-        if (!value) return true
-        return data.label.indexOf(value) !== -1
-      },
-
-      nodeExpand(data) {
-        let aChildren = data.children
-        if (aChildren.length > 0) {
-          this.oExpandedKey[data.id] = true
-          this.oTreeNodeChildren[data.id] = aChildren
-        }
-        this.setExpandedKeys()
-      },
-      nodeCollapse(data) {
-        this.oExpandedKey[data.id] = false
-        // 如果有子节点
-        this.treeRecursion(this.oTreeNodeChildren[data.id], (oNode) => {
-          this.oExpandedKey[oNode.id] = false
+    handlerEdit() {
+      if (this.form.testId) {
+        this.formEdit = false;
+        this.formStatus = "update";
+      }
+    },
+    handlerAdd() {
+      //this.resetForm()
+      this.formEdit = false;
+      this.formStatus = "create";
+    },
+    handleDelete() {
+      this.$confirm("此操作将永久删除, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(() => {
+        delObj(this.currentId).then(() => {
+          this.getList();
+          this.resetForm();
+          this.onCancel();
+          this.$notify({
+            title: "成功",
+            message: "删除成功",
+            type: "success",
+            duration: 2000
+          });
         });
-        this.setExpandedKeys()
-      },
-      setExpandedKeys() {
-        let oTemp = this.oExpandedKey
-        this.aExpandedKeys = []
-        for (let sKey in oTemp) {
-          if (oTemp[sKey]) {
-            this.aExpandedKeys.push(parseInt(sKey));
-          }
-        }
-      },
-      treeRecursion(aChildren, fnCallback) {
-        if (aChildren) {
-          for (let i = 0; i < aChildren.length; ++i) {
-            let oNode = aChildren[i]
-            fnCallback && fnCallback(oNode)
-            this.treeRecursion(oNode.children, fnCallback)
-          }
-        }
-      },
-
-      getNodeData(data) {
-        if (!this.formEdit) {
-          this.formStatus = 'update'
-        }
-        getObj(data.id).then(response => {
-          this.form = response.data.data
-        })
-        this.currentId = data.id
-        this.showElement = true
-      },
-      handlerEdit() {
-        if (this.form.testId) {
-          this.formEdit = false
-          this.formStatus = 'update'
-        }
-      },
-      handlerAdd() {
-        //this.resetForm()
-        this.formEdit = false
-        this.formStatus = 'create'
-      },
-      handleDelete() {
-        this.$confirm('此操作将永久删除, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).then(() => {
-          delObj(this.currentId).then(() => {
-            this.getList()
-            this.resetForm()
-            this.onCancel()
-            this.$notify({
-              title: '成功',
-              message: '删除成功',
-              type: 'success',
-              duration: 2000
-            })
-          })
-        })
-      },
-      update() {
-        putObj(this.form).then(() => {
-          this.getList()
-          this.$notify({
-            title: '成功',
-            message: '更新成功',
-            type: 'success',
-            duration: 2000
-          })
-        })
-      },
-      create() {
-        Vue.set(this.form,"parentId",this.currentId)
+      });
+    },
+    update() {
+      putObj(this.form).then(() => {
+        this.getList();
+        this.$notify({
+          title: "成功",
+          message: "更新成功",
+          type: "success",
+          duration: 2000
+        });
+      });
+    },
+    create() {
+      if (this.form.testId == undefined) {
+        alert("请先选择一个节点！！！");
+        this.reload();
+      } else {
+        Vue.set(this.form, "parentId", this.currentId);
         addObj(this.form).then(() => {
-          this.getList()
+          this.getList();
           this.$notify({
-            title: '成功',
-            message: '创建成功',
-            type: 'success',
+            title: "成功",
+            message: "创建成功",
+            type: "success",
             duration: 2000
-          })
-        })
-      },
-      onCancel() {
-        this.formEdit = true
-        this.formStatus = ''
-      },
-      resetForm() {
-        this.form = {
-          parentId: this.currentId,
-        }
+          });
+        });
       }
+    },
+    onCancel() {
+      this.formEdit = true;
+      this.formStatus = "";
+    },
+    resetForm() {
+      this.form = {
+        parentId: this.currentId
+      };
     }
   }
+};
 </script>
 
