e4229f95014871f5c948cf30784a9a2445b17016
wang <1207234910@qq.com>
2019-11-11 09:20:11 +0800

    2019年11月11日 09:19:51 王晓冬提交 ，补充代码

---
+++ CODE/gjk/gjk-project/gjk-project-biz/src/main/java/com/inforbus/gjk/pro/service/BaseTemplateService.java
@@ -0,0 +1,16 @@
+package com.inforbus.gjk.pro.service;
+
+import com.inforbus.gjk.admin.api.entity.BaseTemplate;
+import com.inforbus.gjk.common.core.util.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+/*
+* 使用Feign调用upms服务
+* 使用BaseTemplateController中的方法
+* */
+@FeignClient("gjk-upms")
+public interface BaseTemplateService {
+    @GetMapping(value = "/basetemplate/{tempId}")
+    public R<BaseTemplate> getById(@PathVariable("tempId") String tempId);
+}
