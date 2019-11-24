7368e6fd8bf72cd8255a4f97ddafede8fe054abb
liu-tf <liutianfeng127001@163.com>
2019-11-24 20:51:54 +0800

    修改表单验证bug

--- CODE/gjk-ui/src/views/libs/software/storageApply.vue
+++ CODE/gjk-ui/src/views/libs/software/storageApply.vue
@@ -5,7 +5,7 @@
         <span>是否将以下软件框架入库及相关文件提交入库？</span>
       </el-header>
       <el-main>
-        <el-form size="mini" label-position="right" label-width="120px" :model="form" :rules="projectRules">
+        <el-form size="mini" label-position="right" label-width="120px" ref="form" :model="form" :rules="projectRules">
           <el-form-item label="请选择审批人" prop="applyUser">
             <el-select v-model="form.applyUser" placeholder="请选择">
               <el-option
@@ -34,7 +34,7 @@
 
     <span slot="footer" class="dialog-footer">
       <el-button @click="dialogStateShow(false)">取 消</el-button>
-      <el-button type="primary" @click="storageApplySoftware">入库</el-button>
+      <el-button type="primary" @click="storageApplySoftware('form')">入库</el-button>
     </span>
   </el-dialog>
 </template>
@@ -119,8 +119,8 @@ export default {
       });
     },
     //提交入库的方法
-    storageApplySoftware() {
-        this.$refs.form.validate((valid, object) => {
+    storageApplySoftware(val) {
+        this.$refs[val].validate((valid, object) => {
             if (valid) {
                 let approval = {};
                 approval.userId = this.userInfo.userId;
