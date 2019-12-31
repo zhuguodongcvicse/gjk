bd8609573aaca46a091f647e3f707a76ab80596f
wang <1207234910@qq.com>
2019-12-31 11:09:59 +0800

    2019年12月31日 11:08:51 王晓冬提交 ，基础模板配置新增控制流程建模页面属性是否显示控制字段

--- CODE/gjk-ui/src/views/admin/basetemplate/addTemplate.vue
+++ CODE/gjk-ui/src/views/admin/basetemplate/addTemplate.vue
@@ -331,6 +331,16 @@
                 ></el-switch>
               </template>
             </el-table-column>
+            <!--在流程建模页面是否显示-->
+            <el-table-column label="流程显示">
+              <template slot-scope="scope">
+                <el-switch
+                  v-model="scope.row.isProcessShow"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949"
+                ></el-switch>
+              </template>
+            </el-table-column>
             <!--属性的动作-->
             <el-table-column label="动作">
               <template slot-scope="scope">
@@ -847,6 +857,7 @@ export default {
         attrMappingName: "",
         actionType: "",
         isShow: true,
+        isProcessShow:false,
         attrKeys: undefined,
         attrConfigType: "inputComm",
         dataKey: "[]",
@@ -1648,6 +1659,7 @@ export default {
                     attrMappingName: i,
                     actionType: "",
                     isShow: true,
+                    isProcessShow:false,
                     attrKeys: undefined,
                     attrConfigType: "inputComm",
                     multiple: false,
@@ -1695,6 +1707,10 @@ export default {
                 //如果有attrs对象,判断是否有multiple字段
                 if (i.multiple == undefined) {
                   Vue.set(i, "multiple", false);
+                }
+                //如果有attrs对象,判断是否有isProcessShow字段
+                if (i.isProcessShow == undefined) {
+                  Vue.set(i, "isProcessShow", false);
                 }
               }
             }
@@ -1774,6 +1790,7 @@ export default {
               attrMappingName: attribute,
               actionType: "",
               isShow: true,
+              isProcessShow:false,
               attrKeys: undefined,
               attrConfigType: "inputComm",
               mappingData: []
--- CODE/gjk-ui/src/views/admin/basetemplate/editTemplate.vue
+++ CODE/gjk-ui/src/views/admin/basetemplate/editTemplate.vue
@@ -331,6 +331,16 @@
                 ></el-switch>
               </template>
             </el-table-column>
+             <!--在流程建模页面是否显示-->
+            <el-table-column label="流程显示">
+              <template slot-scope="scope">
+                <el-switch
+                  v-model="scope.row.isProcessShow"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949"
+                ></el-switch>
+              </template>
+            </el-table-column>
             <!--属性的动作-->
             <el-table-column label="动作">
               <template slot-scope="scope">
@@ -845,6 +855,7 @@ export default {
         attrMappingName: "",
         actionType: "",
         isShow: true,
+        isProcessShow:false,
         attrKeys: undefined,
         attrConfigType: "inputComm",
         dataKey: "[]",
@@ -1646,6 +1657,7 @@ export default {
                     attrMappingName: i,
                     actionType: "",
                     isShow: true,
+                    isProcessShow:false,
                     attrKeys: undefined,
                     attrConfigType: "inputComm",
                     multiple: false,
@@ -1693,6 +1705,10 @@ export default {
                 //如果有attrs对象,判断是否有multiple字段
                 if (i.multiple == undefined) {
                   Vue.set(i, "multiple", false);
+                }
+                //如果有attrs对象,判断是否有isProcessShow字段
+                if (i.isProcessShow == undefined) {
+                  Vue.set(i, "isProcessShow", false);
                 }
               }
             }
@@ -1772,6 +1788,7 @@ export default {
               attrMappingName: attribute,
               actionType: "",
               isShow: true,
+              isProcessShow:false,
               attrKeys: undefined,
               attrConfigType: "inputComm",
               mappingData: []
