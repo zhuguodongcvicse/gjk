f410d5ebf52c9244e29e3fbee27779c8bdb375c3
zhanghongxucoder <zhanghongxucoder@github.com>
2020-02-20 10:03:43 +0800

    删除log2

--- CODE/gjk/gjk-simulation/src/main/java/com/inforbus/gjk/simulation/service/impl/SimulatorServiceImpl.java
+++ CODE/gjk/gjk-simulation/src/main/java/com/inforbus/gjk/simulation/service/impl/SimulatorServiceImpl.java
@@ -32,6 +32,7 @@ import java.util.Set;
 
 /**
  * 仿真业务实现
+ *
  * @Auth l_tf
  */
 @Service
@@ -68,31 +69,33 @@ public class SimulatorServiceImpl implements SimulatorService {
     //获取控制表头文件路径
     @Value("${ctrl.tab.file.path}")
     private String tabFilePath;
+
     //开始仿真客户线程放入全局变量
     @Override
     public boolean startSimulator(String username, List<String> componentLinks, String filePath) {
         //定义通道名
-        String channelName = username+"SimulatorChannel";
+        String channelName = username + "SimulatorChannel";
         //队列初始状态
-        redisTemplate.opsForValue().set(username+":initState","1");
+        redisTemplate.opsForValue().set(username + ":initState", "1");
         //初始化监听对象
         Subscriber subscriber = new Subscriber();
         subscriber.setQueueSize(Integer.parseInt(queueSize));
         subscriber.setUsername(username);
         subscriber.setRedisTemplate(redisTemplate);
         //启动监听线程
-        new SubscriberThread(subscriber,channelName,host).start();
+        new SubscriberThread(subscriber, channelName, host).start();
         //启用客户线程，传入参数
         MoniRecvDataThread startMoniRecvDataThread = ExternalIOTransUtils.startMoniRecvDataThread(host, channelName, componentLinks, filePath, tabFilePath);
         //放入全局变量
-        Global.USERS_SIMULATOR_THREAD.put(username,startMoniRecvDataThread);
+        Global.USERS_SIMULATOR_THREAD.put(username, startMoniRecvDataThread);
         return true;
     }
+
     //停止仿真
     @Override
     public boolean stopSimulator(String username) {
         MoniRecvDataThread moniRecvDataThread = (MoniRecvDataThread) Global.USERS_SIMULATOR_THREAD.get(username);
-        if(moniRecvDataThread == null){
+        if (moniRecvDataThread == null) {
             return false;
 
         }
@@ -103,18 +106,18 @@ public class SimulatorServiceImpl implements SimulatorService {
             redisTemplate.delete(key);
         }
         Set<String> keys1 = redisTemplate.keys(username + ":initState:*");
-        keys1.stream().forEach(key->{
+        keys1.stream().forEach(key -> {
             redisTemplate.delete(key);
         });
         return true;
     }
 
     @Override
-    public Map<String,Object> getData(String username, String projectId,SimulationDTO simulationDTO) {
+    public Map<String, Object> getData(String username, String projectId, SimulationDTO simulationDTO) {
         //修改队列初始化状态
-        redisTemplate.opsForValue().set(username+":initState:"+simulationDTO.getSymbol(),"1");
+        redisTemplate.opsForValue().set(username + ":initState:" + simulationDTO.getSymbol(), "1");
         //拼接队列key
-        String key = "Simulator:" + username+ ":" + simulationDTO.getSymbol();
+        String key = "Simulator:" + username + ":" + simulationDTO.getSymbol();
         //从队列右侧弹出一个元素
         String str = redisTemplate.opsForList().rightPop(key);
 
@@ -124,7 +127,7 @@ public class SimulatorServiceImpl implements SimulatorService {
         String FilePath = managerServiceImpl.getprocessFile(projectId);
         String packinfoFileName = gitDetailPath + FilePath + generateCodeResult + "/packinfo.xml";
 
-        String arrowInfo = simulationDTO.getStartId() +":"+simulationDTO.getStartName()+ "|" + simulationDTO.getEndId()+":"+simulationDTO.getEndName();
+        String arrowInfo = simulationDTO.getStartId() + ":" + simulationDTO.getStartName() + "|" + simulationDTO.getEndId() + ":" + simulationDTO.getEndName();
 
         //获取结构体表格数据
         String[] tabNames = ((String) objects.get("tabNameList")).split("\\|");
@@ -133,88 +136,87 @@ public class SimulatorServiceImpl implements SimulatorService {
         tableData.addAll(forEachGetSimulationTableData((Map) objects.get(tabNames[0])));
         tableData.addAll(forEachGetSimulationTableData((Map) objects.get(tabNames[1])));
         //获取最大xyz维度
-        Map<String,String> MaxXYZ =  ExternalIOTransUtils.getMaxXYZ(FilePath,packinfoFileName,objects,arrowInfo);
-    if(simulationDTO.getX() == null){
-        //获取最大维度值添加到配置页面数据中
-        Map<String,Object> packDataMap = null;
-        packDataMap.put("X","1X");
-        packDataMap.put("Y","1Y");
-        packDataMap.put("Z","1Z");
-        packDataMap.put("Symbol",simulationDTO.getSymbol());
-        packDataMap.put("DataProecssingType",simulationDTO.getDataProecssingType());
-        Map<String, Object>  dataInfo   =ExternalIOTransUtils.parseMoniData(FilePath,packinfoFileName,packDataMap,arrowInfo);
-        Map<String, Object> dataMap = Maps.newHashMap();
-        //表格数据
-        dataMap.put("tableData", tableData);
-        //展示数据
-        dataMap.put("Data", dataInfo);
-        //xyz最大值
-        dataMap.put("MaxXYZ" ,MaxXYZ);
-        return dataMap;
-    }else {
-        Map<String,Object> packDataMap = null;
-        packDataMap.put("X",simulationDTO.getX());
-        packDataMap.put("Y",simulationDTO.getY());
-        packDataMap.put("Z",simulationDTO.getZ());
-        packDataMap.put("Symbol",simulationDTO.getSymbol());
-        packDataMap.put("DataProecssingType",simulationDTO.getDataProecssingType());
-        Map<String, Object>  dataInfo   =ExternalIOTransUtils.parseMoniData(FilePath,packinfoFileName,packDataMap,arrowInfo);
-        Map<String, Object> dataMap = Maps.newHashMap();
-        //表格数据
-        dataMap.put("tableData", tableData);
-        //展示数据
-        dataMap.put("Data", dataInfo);
-        //xyz最大值
-        dataMap.put("MaxXYZ" ,MaxXYZ);
-        return dataMap;
-    }
-
-
-
+        Map<String, String> MaxXYZ = ExternalIOTransUtils.getMaxXYZ(FilePath, packinfoFileName, objects, arrowInfo);
+        if (simulationDTO.getX() == null) {
+            //获取最大维度值添加到配置页面数据中
+            Map<String, Object> packDataMap = null;
+            packDataMap.put("X", "1X");
+            packDataMap.put("Y", "1Y");
+            packDataMap.put("Z", "1Z");
+            packDataMap.put("Symbol", simulationDTO.getSymbol());
+            packDataMap.put("DataProecssingType", simulationDTO.getDataProecssingType());
+            Map<String, Object> dataInfo = ExternalIOTransUtils.parseMoniData(FilePath, packinfoFileName, packDataMap, arrowInfo);
+            Map<String, Object> dataMap = Maps.newHashMap();
+            //表格数据
+            dataMap.put("tableData", tableData);
+            //展示数据
+            dataMap.put("Data", dataInfo);
+            //xyz最大值
+            dataMap.put("MaxXYZ", MaxXYZ);
+            return dataMap;
+        } else {
+            Map<String, Object> packDataMap = null;
+            packDataMap.put("X", simulationDTO.getX());
+            packDataMap.put("Y", simulationDTO.getY());
+            packDataMap.put("Z", simulationDTO.getZ());
+            packDataMap.put("Symbol", simulationDTO.getSymbol());
+            packDataMap.put("DataProecssingType", simulationDTO.getDataProecssingType());
+            Map<String, Object> dataInfo = ExternalIOTransUtils.parseMoniData(FilePath, packinfoFileName, packDataMap, arrowInfo);
+            Map<String, Object> dataMap = Maps.newHashMap();
+            //表格数据
+            dataMap.put("tableData", tableData);
+            //展示数据
+            dataMap.put("Data", dataInfo);
+            //xyz最大值
+            dataMap.put("MaxXYZ", MaxXYZ);
+            return dataMap;
+        }
     }
 
     @Override
-    public Map<String, List<String>> suspend(String username,List<String> symbols) {
+    public Map<String, List<String>> suspend(String username, List<String> symbols) {
         ListOperations<String, String> operations = redisTemplate.opsForList();
         Map<String, List<String>> symbolFrameSelect = Maps.newHashMap();
         List<String> selectData = Lists.newArrayList();
         for (String symbol : symbols) {
-            String key = "Simulator:"+username+":"+symbol;
+            String key = "Simulator:" + username + ":" + symbol;
             Long size = operations.size(key);
             List<String> range = operations.range(key, 0, size - 1);
             for (String s : range) {
                 JSONObject dataMap = JSONUtil.parseObj(s);
-                selectData.add(dataMap.get("frameNum")+"");
+                selectData.add(dataMap.get("frameNum") + "");
             }
-            symbolFrameSelect.put(symbol,selectData);
+            symbolFrameSelect.put(symbol, selectData);
         }
         return symbolFrameSelect;
     }
+
     //点击小图标获取数据源获取
     @Override
-    public Map<String,Object> getDataSource(String username, SimulationDTO simulationDto) {
+    public Map<String, Object> getDataSource(String username, SimulationDTO simulationDto) {
 
         MoniRecvDataThread moniRecvDataThread = (MoniRecvDataThread) Global.USERS_SIMULATOR_THREAD.get(username);
 
         //调用客户接口 获取数据源
         HashMap<String, Object> Data = new HashMap<>();
         List<SysDict> dataProcessingType = sysDictMapper.getDictTypes();
-     //   Data.put("data",moniRecvDataThread.getArrowIdList(simulationDto.getStartId() + "|" + simulationDto.getEndId()));
-        Data.put("sourceData","数据源");
-        Data.put("dataProcessingType",dataProcessingType);
+           Data.put("sourceData",moniRecvDataThread.getArrowIdList(simulationDto.getStartId() + "|" + simulationDto.getEndId()));
+      //  Data.put("sourceData", "数据源");
+        Data.put("dataProcessingType", dataProcessingType);
         return Data;
     }
 
     /**
      * 解析表格Map
-     * @param. 表格数据Map 【key: name, value: 值|备注】
+     *
      * @return
+     * @param. 表格数据Map 【key: name, value: 值|备注】
      */
-    public List<SimulationTableDataDTO> forEachGetSimulationTableData(Map<String, String> tableDataMap){
+    public List<SimulationTableDataDTO> forEachGetSimulationTableData(Map<String, String> tableDataMap) {
         List<SimulationTableDataDTO> simulationTableDataDTOS = Lists.newArrayList();
         SimulationTableDataDTO simulationTableDataDTO = null;
         for (String key : tableDataMap.keySet()) {
-             simulationTableDataDTO = new SimulationTableDataDTO();
+            simulationTableDataDTO = new SimulationTableDataDTO();
             simulationTableDataDTO.setName(key);
             //根据“|”截取值和备注
             String[] valueAndRemake = tableDataMap.get(key).split("\\|");
