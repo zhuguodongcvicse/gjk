61fd7ba359eb6039d469ea8909166cfde39d7a07
dong <1207234910@qq.com>
2020-04-20 10:07:12 +0800

    晓冬提交，解决编译项目启动不了问题

--- CODE/gjk/gjk-compile/src/main/java/com/inforbus/gjk/compile/feign/RemoteDevenvService.java
+++ CODE/gjk/gjk-compile/src/main/java/com/inforbus/gjk/compile/feign/RemoteDevenvService.java
@@ -1,8 +1,8 @@
 package com.inforbus.gjk.compile.feign;
 
-import com.inforbus.gjk.admin.api.feign.factory.RemoteBaseTemplateServiceFallbackFactory;
 import com.inforbus.gjk.common.core.constant.ServiceNameConstants;
 import com.inforbus.gjk.common.core.util.R;
+import com.inforbus.gjk.compile.feign.factory.RemoteDevenvServiceFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +16,7 @@ import java.util.Map;
  * @date 2020/4/16
  * @Description 远程调用的fegin接口，调用编译功能
  */
-@FeignClient(value = ServiceNameConstants.COMPILE_SERVICE,url = "localhost:8080/dataCenter/devenv",fallbackFactory = RemoteBaseTemplateServiceFallbackFactory.class)
+@FeignClient(value = ServiceNameConstants.COMPILE_SERVICE,url = "localhost:8080/dataCenter/devenv",fallbackFactory = RemoteDevenvServiceFallbackFactory.class)
 public interface RemoteDevenvService {
 
     @PutMapping(value = "/Command")
--- CODE/gjk/gjk-compile/src/main/java/com/inforbus/gjk/compile/feign/factory/RemoteDevenvServiceFallbackFactory.java
+++ CODE/gjk/gjk-compile/src/main/java/com/inforbus/gjk/compile/feign/factory/RemoteDevenvServiceFallbackFactory.java
@@ -3,6 +3,7 @@ package com.inforbus.gjk.compile.feign.factory;
 import com.inforbus.gjk.compile.feign.RemoteDevenvService;
 import com.inforbus.gjk.compile.feign.fallback.RemoteDevenvServiceFallbackImpl;
 import feign.hystrix.FallbackFactory;
+import org.springframework.stereotype.Component;
 
 /**
  * RemoteDevenvServiceFallbackFactory
@@ -11,6 +12,7 @@ import feign.hystrix.FallbackFactory;
  * @date 2020/4/17
  * @Description fegin调用失败的熔断器
  */
+@Component
 public class RemoteDevenvServiceFallbackFactory implements FallbackFactory<RemoteDevenvService> {
     @Override
     public RemoteDevenvService create(Throwable throwable) {
