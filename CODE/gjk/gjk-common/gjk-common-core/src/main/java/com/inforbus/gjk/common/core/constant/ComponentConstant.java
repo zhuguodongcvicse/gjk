2b75a1281cf1e750117b0931cd8dbd23bd0a00df
xiaohe2510 <xiaohe2510@163.com>
2020-05-08 09:27:44 +0800

    构件常量类

---
+++ CODE/gjk/gjk-common/gjk-common-core/src/main/java/com/inforbus/gjk/common/core/constant/ComponentConstant.java
@@ -0,0 +1,37 @@
+package com.inforbus.gjk.common.core.constant;
+/**
+ * @ClassName: ComponentConstant
+ * @Desc 构件的常量
+ * @Author xiaohe
+ * @DateTime 2020年5月6日  
+ */
+public interface ComponentConstant {
+	/**
+	  * @Fields COMP_TEST : 测试文件
+	  */
+	String COMP_TEST = "testfile";
+	/**
+	  * @Fields COMP_ALGORITHM : 算法文件
+	  */
+	String COMP_ALGORITHM = "algorithmfile";
+	/**
+	  * @Fields COMP_PLATFORM : 平台文件
+	  */
+	String COMP_PLATFORM = "platformfile";
+	/**
+	  * @Fields COMP_IMG : 图标文件
+	  */
+	String COMP_IMG = "imgfile";
+	/**
+	  * @Fields COMP : 构件token
+	  */
+	String COMP = "component";
+	/**
+	  * @Fields COMP_XML : 构件文件类型
+	  */
+	String COMP_XML = ".XML";
+	
+	
+	
+	
+}
